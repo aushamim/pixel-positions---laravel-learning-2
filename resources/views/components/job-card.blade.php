c45090439d59363ee81fbd9bfb43486f6c745632
aushamim <au.shamim0@gmail.com>
2025-07-29 17:51:14 +0600

    first commit

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,20 @@
+<div class="p-4 bg-gray-100 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">
+        Laracasts
+    </div>
+
+    <div class="py-8 font-bold">
+        <h3>Video Producer</h3>
+        <p>Full Time - From $60,000</p>
+    </div>
+
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <a href="#" class="bg-gray-300/50 hover:bg-gray-300 text-gray-700 px-2 py-1 rounded-xl text-xs font-semibold duration-300">Tag</a>
+            <a href="#" class="bg-gray-300/50 hover:bg-gray-300 text-gray-700 px-2 py-1 rounded-xl text-xs font-semibold duration-300">Tag</a>
+            <a href="#" class="bg-gray-300/50 hover:bg-gray-300 text-gray-700 px-2 py-1 rounded-xl text-xs font-semibold duration-300">Tag</a>
+        </div>
+
+        <img src="https://placehold.co/42x42" alt="Job Image" class="rounded-xl">
+    </div>
+</div>
---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pixel Positions</title>
+
+    {{-- Resources --}}
+    @vite(['resources/js/app.js'])
+</head>
+
+<body>
+    <nav class="px-40 flex justify-between items-center border-b-2 border-gray-100">
+        <div>
+            <a href="/" class="flex items-center">
+                <img class="size-14" src="{{ Vite::asset('resources/images/logo.png') }}" alt="Pixel Positions Logo">
+                <p class="text-xl font-semibold text-purple-700">PixPos</p>
+            </a>
+        </div>
+
+        <div class="flex items-center gap-6 font-semibold text-gray-600">
+            <a href="#" class="hover:text-purple-500 duration-300">Jobs</a>
+            <a href="#" class="hover:text-purple-500 duration-300">Careers</a>
+            <a href="#" class="hover:text-purple-500 duration-300">Salaries</a>
+            <a href="#" class="hover:text-purple-500 duration-300">Companies</a>
+        </div>
+
+        <div><a href="">Post a Job</a></div>
+    </nav>
+
+    <main class="mt-10 max-w-[1200px] mx-auto">
+        {{ $slot }}
+    </main>
+</body>
+
+</html>
